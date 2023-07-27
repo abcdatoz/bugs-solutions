d344275453d67d7ff04027e13b22b395bcbcea92
rey <rhernandez@orfis.gob.mx>
2023-07-27 11:25:15 -0600

    dsds

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -6,6 +6,18 @@ const Holo = () => (
     
         <div>
             <h3>this the Xolos Home</h3>
+                <li>Cedulas ASF 001</li>
+    <li>Cedulas ASF 002</li>
+    <li>Cedulas ASF 003</li>
+    <li>Cedulas ASF 004</li>
+    <li>Cedulas ASF 005</li>
+    <li>Cedulas ASF 006</li>
+    <li>Cedulas ASF 007</li>
+    <li>Cedulas ASF 009</li>
+    https://drive.google.com/file/d/18-oed_HeTIsYSRh4fICZrw0EQZ12LNxD/view
+    https://drive.google.com/file/d/1_0y0a6LlgbsnrncmktXfnd18hk4cYpmf/view/2BzbPS
+    https://download.microsoft.com/download/0/B/8/0B8EFDD9-8CE5-405C-A28D-A108A63AE2FC/AccessDatabaseEngine_X64.exe
+    https://drive.google.com/file/d/1Q1OxwA4iWHnp0PzBtCVnwaDGGZr106eT/view?usp=sharing
         </div>
 )
 
